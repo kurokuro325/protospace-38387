14c0ee5357bfc79bed5778c3f88ba4705985c6d7
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-17 20:49:07 +0900

    エラーの訂正2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -32,11 +32,10 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
-      render :show
+      render :edit
     end
   end
 
